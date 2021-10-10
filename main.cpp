03e444ee532dd95b31bd065deac0360a12b3628d
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-10 22:26:56 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// For C++11 and later versions
+string str1 = "45"; 
+string str2 = "3.14159"; 
+string str3 = "31337 geek"; 
+
+int myint1 = stoi(str1); 
+int myint2 = stoi(str2); 
+int myint3 = stoi(str3); 
+
+// Output
+stoi("45") is 45
+stoi("3.14159") is 3
+stoi("31337 geek") is 31337 
